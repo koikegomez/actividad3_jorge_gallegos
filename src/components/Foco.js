c3aaaad06540509a4223033733dc321735185ea1
Jorge Gallegos <a1121250017@utch.edu.mx>
2022-02-17 16:11:26 -0700

    Primera version de foco encedido

---
+++ src/components/Foco.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import {View, Image, TouchableOpacity} from 'react-native';
+const Foco = () => {
+
+  const [estatus, setEstatus] = useState(false);
+
+  return (
+    <View>
+        <TouchableOpacity onPress={()=> {setEstatus(!estatus) } }>
+        <Image source={estatus ? require('../../img/focoOn.png'): require('../../img/focoOff.png')}
+          style={{width: 150, height: 250}}/>
+        </TouchableOpacity>
+    </View>
+  )
+}
+
+export default Foco
